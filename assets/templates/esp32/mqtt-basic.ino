87c1b914744ce0d81c3af1e8d0af8a1b5c906901
Azzar Budiyanto <azzar_budiyanto@example.com>
2026-02-09 19:20:16 +0000

    feat(templates): upgrade esp32 mqtt template and cli wrapper

--- assets/templates/esp32/mqtt-basic.ino
+++ assets/templates/esp32/mqtt-basic.ino
@@ -1,46 +1,47 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "secrets.h"
 
-// WiFi Config
-const char* ssid = "SSID_NAME";
-const char* password = "SSID_PASS";
-
-// MQTT Config
-const char* mqtt_server = "broker.hivemq.com";
-const int mqtt_port = 1883;
+// MQTT topic config (device-specific; override or set from secrets if needed)
 const char* mqtt_topic_sub = "device/command";
 const char* mqtt_topic_pub = "device/status";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+unsigned long lastReconnectAttempt = 0;
+const unsigned long reconnectIntervalMs = 5000;
+
 void setup_wifi() {
     delay(10);
-    WiFi.begin(ssid, password);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
     }
 }
 
-void reconnect() {
-    while (!client.connected()) {
-        if (client.connect("ESP32Client")) {
-            client.subscribe(mqtt_topic_sub);
-        } else {
-            delay(5000);
-        }
+boolean tryReconnect() {
+    if (client.connect("ESP32Client", MQTT_USER, MQTT_PASS)) {
+        client.subscribe(mqtt_topic_sub);
+        return true;
     }
+    return false;
 }
 
 void setup() {
     Serial.begin(115200);
     setup_wifi();
-    client.setServer(mqtt_server, mqtt_port);
+    client.setServer(MQTT_SERVER, MQTT_PORT);
 }
 
 void loop() {
     if (!client.connected()) {
-        reconnect();
+        unsigned long now = millis();
+        if (now - lastReconnectAttempt >= reconnectIntervalMs) {
+            lastReconnectAttempt = now;
+            tryReconnect();
+        }
+    } else {
+        client.loop();
     }
-    client.loop();
 }
